9ab6f6e42fb96ca4572456f4ec0834085a04ff0c
Hecman80803 <147789917+Hecman80803@users.noreply.github.com>
2023-10-12 23:00:23 -0600

    Creacion de practica

---
+++ src/poop1/POOP1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poop1;
+
+/**
+ * 
+ * @author hecman
+ */
+public class POOP1 {
+
+    /**
+     * Imprime hola mundo
+     * @return
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+    
+}
